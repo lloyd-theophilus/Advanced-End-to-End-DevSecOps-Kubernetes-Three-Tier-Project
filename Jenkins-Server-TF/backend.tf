c0fc4fb7756d1407aeea5290bbd3abf3cd8e7695
Lloyd <osabuteylloyd@gmail.com>
2024-03-29 21:22:22 +0000

    updated terraform s3 bucket details

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    bucket         = "cloudhub2"
+    region         = "eu-west-2"
+    key            = "Advanced-End-to-End-DevSecOps-Kubernetes-Three-Tier-Project"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
